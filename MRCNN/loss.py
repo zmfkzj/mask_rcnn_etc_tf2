c7d4ee1e40b72142cb5f95a8917d1e4d415c7cb8
zmfkzj <qlwlal@naver.com>
2023-03-07 21:53:08 +0900

    rpn_class_loss 원상복귀(focal loss 제거)

--- MRCNN/loss.py
+++ MRCNN/loss.py
@@ -19,11 +19,6 @@ class SmoothL1Loss(KL.Layer):
 
 
 class RpnClassLossGraph(KL.Layer):
-    def __init__(self, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
-        super().__init__(trainable, name, dtype, dynamic, **kwargs)
-        self.focal_loss = tf.losses.BinaryFocalCrossentropy(apply_class_balancing=True, from_logits=True, reduction=tf.losses.Reduction.NONE)
-
-
     def call(self, rpn_match, rpn_class_logits):
         """RPN anchor classifier loss.
 
@@ -42,11 +37,10 @@ class RpnClassLossGraph(KL.Layer):
         rpn_class_logits = tf.gather_nd(rpn_class_logits, indices)
         anchor_class = tf.gather_nd(anchor_class, indices)
         # Cross entropy loss
-        # loss = K.losses.sparse_categorical_crossentropy(anchor_class, rpn_class_logits, from_logits=True)
-        anchor_class = tf.one_hot(anchor_class,2)
-        loss = self.focal_loss(anchor_class, rpn_class_logits)
+        loss = K.losses.sparse_categorical_crossentropy(anchor_class, rpn_class_logits, from_logits=True)
+        # anchor_class = tf.one_hot(anchor_class,2)
+        # loss = self.focal_loss(anchor_class, rpn_class_logits)
         loss = tf.cond(tf.size(loss) > 0, lambda: tf.reduce_mean(loss), lambda: tf.constant(0.0))
-        # loss = tf.cond(tf.size(loss) > 0, lambda: tf.reduce_mean(loss), lambda: np.nan)
         return loss
 
 
@@ -82,7 +76,6 @@ class RpnBboxLossGraph(KL.Layer):
         loss = self.smooth_l1(target_bbox, rpn_bbox)
         
         loss = tf.cond(tf.size(loss) > 0, lambda:tf.reduce_mean(loss), lambda:tf.constant(0.0))
-        # loss = tf.cond(tf.size(loss) > 0, lambda:tf.reduce_mean(loss), lambda:np.nan)
         return loss
 
 
